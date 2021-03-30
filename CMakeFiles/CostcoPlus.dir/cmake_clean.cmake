14cad4e3f727cd370baee083a0bf7d679c4a9c36
Daniel Mulvad <daniel.mulvad@greenwavesystems.com>
2021-03-29 19:28:09 -0700

    cmake

---
+++ CMakeFiles/CostcoPlus.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CostcoPlus_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/CostcoPlus_autogen.dir/ParseCache.txt"
+  "CostcoPlus_autogen"
+  "CMakeFiles/CostcoPlus.dir/CostcoPlus_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/CostcoPlus.dir/main.cpp.o"
+  "CMakeFiles/CostcoPlus.dir/mainwindow.cpp.o"
+  "CostcoPlus"
+  "CostcoPlus.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CostcoPlus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/CostcoPlus.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CostcoPlus.
+# This may be replaced when dependencies are built.

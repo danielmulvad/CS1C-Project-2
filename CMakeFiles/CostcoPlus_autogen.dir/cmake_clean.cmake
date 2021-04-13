dce4f526f249b6bb35d290f95ce775a56a8edb75
Daniel Mulvad <daniel.mulvad@greenwavesystems.com>
2021-04-12 18:57:47 -0700

    changing build tool to qmake - please rebase dev branches

--- CMakeFiles/CostcoPlus_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/CostcoPlus_autogen"
-  "CostcoPlus_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/CostcoPlus_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()

9399a9fbba80878b3de898a7d76c67b9eabf68ae
Rahnya Moubayed <rahnyamoubayed@hotmail.com>
2021-04-21 18:39:05 -0700

    I added a addMember.cpp file and an addMember.h file

---
+++ addMember.cpp
@@ -0,0 +1,19 @@
+#include "addMember.h"
+#include "database.h"
+#include <QSpinBox>
+
+AddMember::AddMember(QWidget *parent) :
+    QDialog(parent), ui(new Ui::AddMember)
+{
+    ui ->setupUi(this);
+}
+
+AddMember::~AddMember()
+{
+    delete ui;
+}
+
+void AddMember::on_addFromFileButton_clicked()
+{
+
+}
---
+++ addMember.h
@@ -0,0 +1,26 @@
+#ifndef ADDMEMBER_H
+#define ADDMEMBER_H
+
+#include "costcopage.h"
+#include <vector>
+#include <QDialog>
+namespace Ui
+{
+class AddMember;
+}
+
+class AddMember : public QDialog
+{
+    Q_OBJECT
+public:
+    explicit AddMember(QWidget *parent = nullptr);
+    ~AddMember();
+
+private slots:
+    void on_pushButton_clicked();
+    void on_addFromFileButton_clicked();
+private:
+    Ui::AddStadium *ui;
+};
+
+#endif // ADDMEMBER_H
